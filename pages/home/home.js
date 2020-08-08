5576406377cd53940417a322d59cf8734be190e1
admin <2076338679@163.com>
2020-08-08 14:23:22 +0800

    邂逅小程序

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,28 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    name:'kobe',
+    age:20,
+    basketballStars:[
+      {id:110, name:'kobe', age:28},
+      {id:111, name:'james', age:26},
+      {id:112, name:'joedan', age:30},
+      {id:110, name:'curry', age:24}
+    ],
+    counter:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleIncrebtion() {
+    //这种方法是错误的，因为它不能进行页面的渲染操作
+    // this.data.counter++;
+    // console.log(this.data.counter);
+    //2.需要使用this.setData()函数
+    this.setData({
+      counter:this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubcrebtion() {
+    this.setData({
+      counter:this.data.counter - 1
+    })
   }
 })
